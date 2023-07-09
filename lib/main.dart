3822b977ce35a1e5122682e42225d48c167cc952
amangex <amangexon@gmail.com>
2023-07-10 04:25:39 +0600

    Widget: Text, text style. Some background color

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,24 @@ void main() {
       appBar: AppBar(
         title: const Text("Home Page"),
          centerTitle: true,
+         backgroundColor: Colors.grey[900],
          ),
-         body: const Text("Press the button below!"),
+         body: const Text(
+          "Press the button below!",
+           style: TextStyle(
+            fontSize: 30.0,
+            fontStyle: FontStyle.italic,
+            fontWeight: FontWeight.bold,
+            decoration: TextDecoration.underline,
+            letterSpacing: 2.0,
+            color: Colors.black87,
+           ),
+          ),
          floatingActionButton: FloatingActionButton(
           onPressed: () {
            const Text("Add");
           }),
+        backgroundColor: Colors.grey[300],
        ),
     ),
   );
