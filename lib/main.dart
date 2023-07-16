0c3bb574c7bd4150d5933b6c5d1d36db847bcf49
amangex <amangexon@gmail.com>
2023-07-17 01:02:10 +0600

    Widgets RichText and TextSpan

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const Fluttertutorialapp());
 
-
-
 class Fluttertutorialapp extends StatelessWidget {
   const Fluttertutorialapp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-    title: "something new",
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
-         centerTitle: true,
-         backgroundColor: Colors.grey[900],
-         ),
-         body: const Text(
-          "Press the button below!",
-           style: TextStyle(
-            fontSize: 30.0,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline,
-            letterSpacing: 2.0,
-            color: Colors.red,
-           ),
+      title: "something new",
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Home Page"),
+            centerTitle: true,
+            backgroundColor: Colors.grey[900],
           ),
-         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-           const Text("Add");
-          }),
-        backgroundColor: Colors.grey[300],
-       ),
+          body: RichText(
+            text: const TextSpan(
+              style: TextStyle(
+                fontSize: 30.0,
+                fontStyle: FontStyle.italic,
+                color: Colors.black,
+              ),
+              children: <TextSpan>[
+                TextSpan(text: "Press ", style: TextStyle(fontSize: 50.0)),
+                TextSpan(
+                    style: TextStyle(color: Colors.green),
+                    children: <TextSpan>[
+                      TextSpan(text: "this "),
+                      TextSpan(
+                          text: "button",
+                          style:
+                              TextStyle(decoration: TextDecoration.underline)),
+                    ]),
+                TextSpan(text: " below!"),
+              ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: const Text("+"),
+            onPressed: () {},
+          ),
+          backgroundColor: Colors.grey[300]),
     );
   }
 }
